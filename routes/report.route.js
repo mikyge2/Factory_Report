8acb37865579f69077e4a3d06f6f690d52c8edd3
Michael Getu <michaelgetuk@yahoo.com>
2025-05-22 16:11:39 +0300

    Excel Download. Include Reporter Name and Plant Report.

--- routes/report.route.js
+++ routes/report.route.js
@@ -16,8 +16,8 @@ reportRouter.get('/', authorize, getReports);
 
 reportRouter.get('/:id', authorize, getReport);
 
-// reportRouter.get('/plant/:name', authorize, getPlantReports);
-reportRouter.get('/plant/:name', authorize, getReports);
+reportRouter.get('/plant/:name', authorize, getPlantReports);
+//reportRouter.get('/plant/:name', authorize, getReports);
 
 reportRouter.get('/user/:id', authorize, getUserReports);
 
